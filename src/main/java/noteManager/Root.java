174ad3cee81623a3e31ddd3665a700a9b7df2f1f
Druxa063 <andru-163@mail.ru>
2018-05-17 21:36:14 +0400

    fix root class

--- src/main/java/noteManager/Root.java
+++ src/main/java/noteManager/Root.java
@@ -1,13 +1,16 @@
 package noteManager;
 
 import noteManager.controller.NoteController;
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Root {
     public static void main(String[] args) {
-        ApplicationContext app = new ClassPathXmlApplicationContext("spring/spring-app.xml");
-        NoteController controller = app.getBean(NoteController.class);
-        controller.getAll().forEach(System.out::println);
+        try (ConfigurableApplicationContext app = new ClassPathXmlApplicationContext("spring/spring-app.xml")){
+            NoteController controller = app.getBean(NoteController.class);
+            controller.getAll().forEach(System.out::println);
+        }
     }
 }
